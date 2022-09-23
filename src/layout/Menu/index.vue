900ede9176cfab1a096e8298dde4e739c0155fec
SeaEliy <185043843@qq.com>
2022-09-23 22:59:12 +0800

    feat(): 🍻 学习vue3-父子组件传参

--- src/layout/Menu/index.vue
+++ src/layout/Menu/index.vue
@@ -1,10 +1,58 @@
 <template>
-  <div class="Menu">菜单区域</div>
+  <div class="Menu">菜单区域
+    <div>
+      {{title}}
+    </div>
+    <div>
+      {{data}}
+    </div>
+    <br>
+    <div>
+      <button v-on:click="clickTap">派发</button>
+      <button v-on:click="clickTap2">派发2</button>
+    </div>
+  </div>
 </template>
 
 <script setup lang='ts'>
 import { ref, reactive } from 'vue'
+// 接收父组件传过来的值
+type Props = {
+  title: string,
+  // 子组件的标识符要与父组件v-bind后面的标识符完全相同，才能传递过来。
+  data: number[]
+}
+// defineProps<Props>()
 
+//----------------------------------
+
+// 子组件给父组件传参需要通过事件进行
+/* 点击 派发按钮时 向父组件派发了一个事件，叫 on-click 然后事件中传了一个list数组  */
+const list = reactive<number[]>([6, 6, 6])
+const flag = ref<boolean>(false)
+const emit = defineEmits(['on-click', 'on-click2'])
+const clickTap = () => {
+  emit('on-click', list)
+}
+const clickTap2 = () => {
+  // 也可以传递多个参数
+  emit('on-click2', list, false)
+}
+
+// 暴露实例上的东西方便子组件拿
+// 这样做比较安全，父组件不能随意修改子组件的内容，你想修改什么再暴露什么就行了
+defineExpose({
+  list,
+  flag
+})
+
+// 子组件想给父组件传递默认值
+// ts才有withDefas这个东西
+withDefaults(defineProps<Props>(), {
+  title: '我是默认值',
+  // 复杂类型通过return
+  data: () => [1, 1, 1, 1]
+})
 </script>
 <style lang='less' scoped>
 .Menu {
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="layout">
-    <Menu></Menu>
+    <!-- 父组件给子组件传值title -->
+    <!-- 传递复杂类型 需要使用v-bind 简写为冒号 : -->
+    <!-- 通过ref拿到子组件的实例 -->
+    <Menu ref="menu" @on-click="getList" @on-click2="getList2" title="我想穿一条裤子" :data="list"></Menu>
     <div class="layout-right">
       <Header></Header>
       <Content></Content>
@@ -13,6 +16,22 @@ import { ref, reactive } from 'vue'
 import Menu from './Menu/index.vue'
 import Content from './Content/index.vue'
 import Header from './Header/index.vue'
+// 传递复杂类型 需要使用v-bind
+const list = reactive<number[]>([1, 2, 3])
+
+// 父组件接收子组件传递过来的参数
+const getList = (list: number) => {
+  console.log(list, "我是子组件传过来的list");
+  // 打印子组件传递过来的实例
+  console.log(menu.value);
+}
+const getList2 = (list: number, flag: boolean) => {
+  console.log(list, "我是子组件传过来的list");
+  console.log(flag, "我是子组件传过来的第二个参数false");
+}
+
+// 通过ref拿到子组件的实例
+const menu = ref(null)
 </script>
 <style lang='less' scoped>
 .layout {
