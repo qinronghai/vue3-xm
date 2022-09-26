fdf2f6a955440d3aa5c1fd393979f0f1367c62ef
SeaEliy <185043843@qq.com>
2022-09-26 23:32:08 +0800

    feat(): ✨ 学习vue3-slot插槽全家桶

---
+++ src/10-动态组件.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app">
+
+    <layout></layout>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref, reactive } from 'vue'
+import layout from './layout/index.vue'
+
+</script>
+<style lang='less' >
+// 初始化样式
+html,
+body,
+#app {
+  overflow: hidden;
+  height: 100%;
+}
+
+.app {
+}
+</style>
---
+++ src/components/Dialog/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="Dialog">
+    <!-- 具名插槽 -->
+    <header class="header">
+      <slot name="header"></slot>
+    </header>
+    <!-- 匿名插槽 -->
+    <main class="main">
+      <div v-for="item in data">
+        <slot :data="item"></slot>
+
+      </div>
+    </main>
+    <!-- 具名插槽 简写 -->
+
+    <footer class="footer">
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref, reactive } from 'vue'
+
+type names = {
+  name: string,
+  age: number
+}
+
+const data = reactive<names[]>([{
+  name: "小满被插入了1",
+  age: 201
+}, {
+  name: "小满被插入了2",
+  age: 202
+}, {
+  name: "小满被插入了3",
+  age: 203
+}, {
+  name: "小满被插入了4",
+  age: 204
+}])
+</script>
+<style lang='less' scoped>
+.Dialog {
+  .header {
+    height: 200px;
+    color: #fff;
+    background-color: red;
+  }
+
+  .main {
+    height: 200px;
+    color: #fff;
+    background-color: green;
+  }
+
+  .footer {
+    height: 200px;
+    color: #fff;
+    background-color: blue;
+  }
+}
+</style>
