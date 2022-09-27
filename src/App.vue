dad83138937654692a89582a992cf9afca197634
SeaEliy <185043843@qq.com>
2022-09-27 12:35:32 +0800

    feat(): ✨ 学习vue3-keep-alive缓存

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,29 @@
 <template>
-  <el-form :model="form" label-width="120px">
-    <el-form-item label="Activity name">
-      <el-input v-model="form.name" />
-    </el-form-item>
-    <el-form-item label="Activity zone">
-      <el-select v-model="form.region" placeholder="please select your zone">
-        <el-option label="Zone one" value="shanghai" />
-        <el-option label="Zone two" value="beijing" />
-      </el-select>
-    </el-form-item>
-    <el-form-item label="Activity time">
-      <el-col :span="11">
-        <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%" />
-      </el-col>
-      <el-col :span="2" class="text-center">
-        <span class="text-gray-500">-</span>
-      </el-col>
-      <el-col :span="11">
-        <el-time-picker v-model="form.date2" placeholder="Pick a time" style="width: 100%" />
-      </el-col>
-    </el-form-item>
-    <el-form-item label="Instant delivery">
-      <el-switch v-model="form.delivery" />
-    </el-form-item>
-    <el-form-item label="Activity type">
-      <el-checkbox-group v-model="form.type">
-        <el-checkbox label="Online activities" name="type" />
-        <el-checkbox label="Promotion activities" name="type" />
-        <el-checkbox label="Offline activities" name="type" />
-        <el-checkbox label="Simple brand exposure" name="type" />
-      </el-checkbox-group>
-    </el-form-item>
-    <el-form-item label="Resources">
-      <el-radio-group v-model="form.resource">
-        <el-radio label="Sponsor" />
-        <el-radio label="Venue" />
-      </el-radio-group>
-    </el-form-item>
-    <el-form-item label="Activity form">
-      <el-input v-model="form.desc" type="textarea" />
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="onSubmit">Create</el-button>
-      <el-button>Cancel</el-button>
-    </el-form-item>
-  </el-form>
+  <div class="app">
+    <el-button type="primary" @click="flag=!flag">切换组件</el-button>
+    <!-- 把组件放到keep-alive中就会自动的缓存我们刚才输入的值 -->
+    <!-- include属性：只想缓存的组件名称 -->
+    <!-- exclude: 不缓存的组件 -->
+    <!-- max='10': 缓存组件最大数为10，是一个算法，会把最不常用的，旧的组件都剔除掉，只剩下十个。 -->
+    <keep-alive>
+      <A v-if="flag"></A>
+      <B v-else></B>
+    </keep-alive>
+    <!-- keep alive  -->
+    <!-- 应用场景：在B组件的输入框中写了一些值，然后切换到别的组件，如A之后再切换回B组件，发现刚刚输入的值不见了，这对于用户体验很不好。 -->
+  </div>
 </template>
 
-<script lang="ts" setup>
-import { reactive } from 'vue'
+<script setup lang='ts'>
+import { ref, reactive, onMounted, onUnmounted, onActivated, onDeactivated } from 'vue'
+import A from "./components/KeepAlive/A.vue";
+import B from "./components/KeepAlive/B.vue";
 
-// do not use same name with ref
-const form = reactive({
-  name: '',
-  region: '',
-  date1: '',
-  date2: '',
-  delivery: false,
-  type: [],
-  resource: '',
-  desc: '',
-})
+const flag = ref<boolean>(false)
 
-const onSubmit = () => {
-  console.log('submit!')
-}
+
 </script>
+<style lang='less' scoped>
+.app {
+}
+</style>
---
+++ src/components/KeepAlive/A.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="A">
+    <h1>我是A组件</h1>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref, reactive, onMounted, onActivated, onDeactivated, onUnmounted } from 'vue'
+onMounted(() => {
+  console.log('初始化');
+})
+
+// 使用keep-alive会增加两个常用的生命周期
+onActivated(() => {
+  console.log('keep-alive初始化');
+
+})
+
+onDeactivated(() => {
+  console.log('keep-alive卸载');
+
+})
+// --------------------------------
+// 使用keep-alive 之后不会走onunmouted 卸载操作
+onUnmounted(() => {
+  console.log('卸载');
+})
+</script>
+<style lang='less' scoped>
+.A {
+}
+</style>
