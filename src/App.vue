b81eb912d5fae4bd9ff077f1d853134912cb42f8
SeaEliy <185043843@qq.com>
2022-09-27 09:40:28 +0800

    feat(): ✨ 学习vue3-Teleport传送组件

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="app">
+    <!-- Teleport传送组件可以可以把一个模板传送到指定的dom元素下 -->
+    <Teleport :disabled="false" to="body">
+      <A></A>
+    </Teleport>
 
-    <layout></layout>
   </div>
 </template>
 
 <script setup lang='ts'>
 import { ref, reactive } from 'vue'
-import layout from './layout/index.vue'
-
+import A from "./components/Teleport/index.vue";
 </script>
 <style lang='less' >
 // 初始化样式
@@ -20,5 +22,8 @@ body,
 }
 
 .app {
+  position: relative;
+  height: 50vh;
+  background-color: yellow;
 }
 </style>
---
+++ src/components/Teleport/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="Dialog">
+    <div class="Dialog-txt">我是弹窗</div>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref, reactive } from 'vue'
+
+</script>
+<style lang='less' scoped>
+.Dialog {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 150px;
+  height: 200px;
+  margin-top: -100px;
+  margin-left: -75px;
+  background-color: black;
+}
+</style>
