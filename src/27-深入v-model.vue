98968e2928d45f5e2589f806612d22512b2b6ca6
SeaEliy <185043843@qq.com>
2022-10-03 12:59:18 +0800

    feat(): ✨ 学习vue3-自定义指令-生命周期钩子

---
+++ src/27-深入v-model.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app">
+    <h1>我是App.vue父组件</h1>
+    <div>isShow:{{isShow}}</div>
+    <div>text:{{text}}</div>
+    <button @click="isShow=!isShow">开关</button>
+    <hr>
+    <!-- v-model= 普通的v-model -->
+    <!-- v-model:多v-model的用法 -->
+    <!-- 自定义修饰符 -->
+    <VModelVue v-model:textVal.isBt="text" v-model="isShow"></VModelVue>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref, reactive } from 'vue'
+import VModelVue from "./components/Vmodel/Vmode.vue";
+const isShow = ref(true)
+const text = ref<string>('小海')
+</script>
+<style lang='less' scoped>
+.app {
+}
+</style>
---
+++ src/components/CustomOrder/A.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="A">
+    <h1>我是A组件</h1>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref, reactive } from 'vue'
+
+</script>
+<style lang='less' scoped>
+.A {
+  width: 200px;
+  height: 200px;
+  border: 2px solid #ccc;
+}
+</style>
