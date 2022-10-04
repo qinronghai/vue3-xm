95df723f1781daee3b13156a1b73eb7d7c715270
SeaEliy <185043843@qq.com>
2022-10-04 10:33:28 +0800

    feat(): ✨ 学习vue3-使用hook

---
+++ src/components/UseHook/A.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="A"></div>
+</template>
+
+<script setup lang='ts'>
+import { ref, reactive } from 'vue'
+import { useAttrs } from 'vue'
+// 使用官方的库，自定义hook去实现接收父组件传过来的值
+const attr = useAttrs()
+console.log(attr);
+</script>
+<style lang='less' scoped>
+.A {
+}
+</style>
