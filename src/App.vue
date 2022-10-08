d61e29cc4c02b46d743a1fda023a112da650f939
SeaEliy <185043843@qq.com>
2022-10-08 14:24:16 +0800

    feat(): ✨ 学习vue3-css新特性

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,44 @@
 <template>
   <div class="app">
-    <div class="bg">
-      <el-input class="ipt"></el-input>
-    </div>
+    <A>
+      <div class="a">私人定制div</div>
+    </A>
+    <div class="ddd">动态Css</div>
+    <div :class="[$style.ccc,$style.border]">module</div>
   </div>
 </template>
 
 <script setup lang='ts'>
 import { ref, reactive } from 'vue'
+import A from './components/CssNewFeater/A.vue'
+const style = ref('red');
+setTimeout(() => {
+  style.value = "#ccc"
+}, 3000);
 
 </script>
 <style lang='less' scoped>
 .app {
-  .bg {
-    .ipt {
-      width: 200px;
+  // 2. 全局的样式
+  // :global(div) {
+  //   color: blue;
+  // }
 
-      :deep(.el-input__inner) {
-        background-color: red;
-      }
-    }
-  }
+  // 3. 动态样式
+  /* .ddd {
+    color: v-bind(style);
+  } */
 }
+</style>
+
+// 4. module的用法
+<style module>
+/* 自定义名称 module='zs'  上面模板上的也要变化 $style.ccc 变成 zs.ccc */
+ .ccc {
+   color: aqua;
+ }
+
+ .border {
+   border: 1px solid #ccc;
+ }
 </style>
---
+++ src/components/CssNewFeater/A.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="A">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref, reactive } from 'vue'
+
+</script>
+<style lang='less' scoped>
+.A {
+  // 1. 在这里修改父级插槽内的样式是不行的
+  :slotted(.a) {
+    // color: red;
+  }
+}
+</style>
