bf037cd6876e5f1f94fee1c7f47650c2659520e0
SeaEliy <185043843@qq.com>
2022-10-09 16:12:10 +0800

    feat(): ✨ 学习vue3-函数式编程-h函数

---
+++ src/36-css原子化-unoCss.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- css原子化 -->
+  <div class="flex red">
+    刺客
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref, reactive } from 'vue'
+</script>
+<style lang='less' scoped>
+.app {
+}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
 <template>
-  <!-- css原子化 -->
-  <div class="flex red">
-    刺客
+  <div class="app">
+    <Btn @on-click="getBtn" text="小海的帽子"></Btn>
   </div>
 </template>
 
 <script setup lang='ts'>
 import { ref, reactive } from 'vue'
+// 函数式编程
+import { h } from "vue";
+
+type Props = {
+  text: string
+}
+
+const Btn = (props: Props, ctx: any) => {
+  return h('div', {
+    class: ['flex', 'red'],
+    onClick: () => {
+      ctx.emit('on-click', '我是按钮')
+    }
+  },
+    props.text
+  )
+}
+
+const getBtn = (str: string) => {
+  console.log(str);
+}
+
 </script>
 <style lang='less' scoped>
 .app {
